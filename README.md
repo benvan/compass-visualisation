85aa97c4b1eaf22395fae08a715f004dbef187f9
Ben van Enckevort <benvan@gmail.com>
2013-02-13 01:42:27 +0000

    server and client working - reading from ardreado. data not normalised yet, but displaying fine realtime

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ compass-visualisation
 =====================
 
 small utility to view output of digital compass - realtime / playback
+
+
+N.B. just in case anybody stumbles upon this - it's pretty much a rip and repurposing of one of the three.js examples, here: http://mrdoob.github.com/three.js/examples/canvas_lines_sphere.html 
---
+++ server.js
@@ -0,0 +1,19 @@
+var sys = require('sys')
+  , spawn = require('child_process').spawn
+  , express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+io.set('log level',2);
+
+server.listen(8000);
+
+app.use(express.static(__dirname));
+
+io.sockets.on('connection', function (socket) {
+  var tail = spawn('tail',["-f","/home/ben/foo"]);
+  tail.stdout.on('data', function(data){
+    socket.send(data.toString('utf-8'));
+  });
+});
