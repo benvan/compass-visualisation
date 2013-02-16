29820821b7b5d71e2185e29d4370656bc790b63a
Ben van Enckevort <benvan@gmail.com>
2013-02-16 18:16:54 +0000

    now using serial port module

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var sys = require('sys')
   , express = require('express')
   , app = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , serial = require('serialport')
+  ;
 
 io.set('log level',2);
 
@@ -11,10 +13,21 @@ server.listen(8000);
 
 app.use(express.static(__dirname));
 
+spawn('ardreado',[]);
+var port = new serial.SerialPort('/dev/ttyACM0', { 
+    baudrate: 9600
+  , parser : serial.parsers.readline('\n')
+});
+
+
 io.sockets.on('connection', function (socket) {
-  //var tail = spawn('tail',["-f","/home/ben/foo"]);
-  var tail = spawn('ardreado',[]);
-  tail.stdout.on('data', function(data){
+  port.on('data', function(data){
+    console.log(data.toString());
     socket.send(data.toString('utf-8'));
   });
+  //var tail = spawn('tail',["-f","/home/ben/foo"]);
+  //var tail = spawn('ardreado',[]);
+  //tail.stdout.on('data', function(data){
+    //socket.send(data.toString('utf-8'));
+  //});
 });
